1a221433327559d3d5ca06b517afd8464ebbfda5
MJ Rossetti <datacreativellc@gmail.com>
2018-10-13 22:03:27 -0400

    Load factory bot config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,27 @@
 require "bundler/setup"
 require "my_banner"
 
+require "factory_bot"
+require "webmock/rspec"
+
+Dir[File.expand_path('support/*.rb', File.dirname(__FILE__))].each do |file|
+  require file
+end # h/t: https://stackoverflow.com/a/21851294/670433
+
 Dir[File.expand_path('support/shared_contexts/*.rb', File.dirname(__FILE__))].each do |file|
   require file
 end # h/t: https://stackoverflow.com/a/21851294/670433
 
-require 'webmock/rspec'
+
+#required_files = Dir[File.expand_path('support/*.rb', File.dirname(__FILE__))]
+#required_files += Dir[File.expand_path('support/shared_contexts/*.rb', File.dirname(__FILE__))]
+#required_files.each do |file|
+#  require file
+#end # h/t: https://stackoverflow.com/a/21851294/670433
+
+
+
+
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
