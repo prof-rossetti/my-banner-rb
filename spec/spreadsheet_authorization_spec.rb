6ad0d5a662f82fdde62649a0d108e2f5c889e48c
MJ Rossetti <datacreativellc@gmail.com>
2018-12-19 20:24:00 -0500

    Not sure why this one test is failing

---
+++ spec/spreadsheet_authorization_spec.rb
@@ -0,0 +1,15 @@
+module MyBanner
+  RSpec.describe SpreadsheetAuthorization do
+
+    it_behaves_like "a GoogleAuthorization" do
+      let(:scope) { "https://www.googleapis.com/auth/spreadsheet" }
+      let(:credentials_filepath) { "spec/mocks/auth/spreadsheet_credentials.json" }
+      let(:token_filepath) { "spec/mocks/auth/spreadsheet_token.yaml" }
+
+      let(:client_id) { "mock-spreadsheet-client-id.apps.googleusercontent.com" }
+      let(:client_secret) { "mock-spreadsheet-client-secret" }
+      let(:refresh_token) { "mock-spreadsheet-refresh-token" }
+    end
+
+  end
+end
--- spec/support/shared_contexts/google_authorization.rb
+++ spec/support/shared_contexts/google_authorization.rb
@@ -1,6 +1,16 @@
-
-
-
+#
+# @example
+#
+#   it_behaves_like "a GoogleAuthorization" do
+#     let(:scope) { "https://www.googleapis.com/auth/calendar" }
+#     let(:credentials_filepath) { "spec/mocks/auth/calendar_credentials.json" }
+#     let(:token_filepath) { "spec/mocks/auth/calendar_token.yaml" }
+#
+#     let(:client_id) { "mock-calendar-client-id.apps.googleusercontent.com" }
+#     let(:client_secret) { "mock-calendar-client-secret" }
+#     let(:refresh_token) { "mock-calendar-refresh-token" }
+#   end
+#
 RSpec.shared_examples "a GoogleAuthorization" do
 
   let(:authorization) { described_class.new(scope: scope, credentials_filepath: credentials_filepath, token_filepath: token_filepath) }
