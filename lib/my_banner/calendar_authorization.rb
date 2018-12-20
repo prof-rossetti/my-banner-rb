2439d95965c617ff482603d1684153ed29009161
MJ Rossetti <datacreativellc@gmail.com>
2018-12-20 14:12:31 -0500

    Remove comments

--- lib/my_banner/calendar_authorization.rb
+++ lib/my_banner/calendar_authorization.rb
@@ -3,8 +3,6 @@ require "google/apis/calendar_v3"
 module MyBanner
   class CalendarAuthorization < GoogleAuthorization
 
-    # permission to read and write ("https://www.googleapis.com/auth/calendar")
-    # @see: https://github.com/googleapis/google-api-ruby-client/blob/6773823e78266830a9a8a651d5fd52e307b63e97/generated/google/apis/calendar_v3.rb#L31-L43
     AUTH_SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
 
     def initialize(options={})
--- lib/my_banner/spreadsheet_authorization.rb
+++ lib/my_banner/spreadsheet_authorization.rb
@@ -3,8 +3,6 @@ require "google/apis/sheets_v4"
 module MyBanner
   class SpreadsheetAuthorization < GoogleAuthorization
 
-    # permission to read and write ("https://www.googleapis.com/auth/calendar")
-    # @see: https://github.com/googleapis/google-api-ruby-client/blob/6773823e78266830a9a8a651d5fd52e307b63e97/generated/google/apis/sheets_v4.rb#L39-L43
     AUTH_SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
 
     def initialize(options={})
