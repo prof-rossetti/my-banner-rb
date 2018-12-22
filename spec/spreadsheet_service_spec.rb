df1be2f5b575fcc2175434284435019894d82b15
MJ Rossetti <datacreativellc@gmail.com>
2018-12-22 12:52:50 -0500

    Test spreadsheet file

--- spec/spreadsheet_service_spec.rb
+++ spec/spreadsheet_service_spec.rb
@@ -3,13 +3,6 @@ module MyBanner
     let(:section){ create(:section) }
     let(:service){ described_class.new(section) }
 
-    #let(:file_list) {
-    #  Google::Apis::DriveV3::FileList.new(files: [
-    #    Google::Apis::DriveV3::File.new(properties: {id: "mock-doc-1", kind: "drive#file", name: "My Spreadshet Document 1", mime_type: "application/vnd.google-apps.spreadsheet"}),
-    #    Google::Apis::DriveV3::File.new(properties: {id: "mock-doc-2", kind: "drive#file", name: "My Spreadshet Document 2", mime_type: "application/vnd.google-apps.spreadsheet"}),
-    #    Google::Apis::DriveV3::File.new(properties: {id: "mock-doc-3", kind: "drive#file", name: service.spreadsheet_title, mime_type: "application/vnd.google-apps.spreadsheet"})
-    #  ] )
-    #}
     let(:file_list) {
       Google::Apis::DriveV3::FileList.new(files: [
         Google::Apis::DriveV3::File.new(id: "mock-doc-1", kind: "drive#file", name: "My Spreadshet Document 1", mime_type: "application/vnd.google-apps.spreadsheet"),
@@ -22,11 +15,6 @@ module MyBanner
     let(:new_spreadsheet) { Google::Apis::SheetsV4::Spreadsheet.new(new_spreadsheet_attrs) }
     let(:spreadsheet_attrs) { { properties: {title: file.name }, spreadsheet_id: file.id } } # sheets: [roster_sheet]
     let(:spreadsheet) { Google::Apis::SheetsV4::Spreadsheet.new(spreadsheet_attrs) }
-
-    #before(:each) do
-    #  allow(file).to receive(:id).and_return(file.id) # allow any instance of Google::Apis::DriveV3::File to respond to #id (b/c that's how it works with the returned object)
-    #  allow(file).to receive(:name).and_return(file.name) # allow any instance of Google::Apis::DriveV3::File to respond to #name
-    #end
 
     describe "@spreadsheet_title" do
       let(:title) { "Gradebook - INFO 101 (201810)" }
@@ -42,12 +30,6 @@ module MyBanner
           allow(service).to receive(:spreadsheet_file).and_return(file)
           allow(service.client).to receive(:get_spreadsheet).with(file.id).and_return(spreadsheet)
         end
-
-        #it "finds the matching file" do
-        #  #allow(service).to receive(:spreadsheet_title).and_return(file.name)
-        #  file.names = file_list.files.map{ |f| f.properties[:name] }
-        #  expect(file.names).to include(service.spreadsheet_title)
-        #end
 
         it "makes a request for the full spreadsheet object" do
           expect(service.client).to receive(:get_spreadsheet).with(file.id).and_return(spreadsheet)
@@ -82,19 +64,22 @@ module MyBanner
       end
     end
 
-    #describe "#spreadsheet_file" do
-    #  before(:each) do
-    #    allow(service).to receive(:file_list).and_return(file_list)
-    #  end
-#
-    #  it "requests all files" do
-    #    expect(service).to receive(:file_list).and_return(file_list)
-    #    service.spreadsheet_file
-    #  end
-#
-    #  it "checks for an existing file with matching title" do
-    #  end
-    #end
+    describe "#spreadsheet_file" do
+      before(:each) do
+        allow(service).to receive(:file_list).and_return(file_list)
+      end
+
+      it "requests all files" do
+        expect(service).to receive(:file_list).and_return(file_list)
+        service.spreadsheet_file
+      end
+
+      it "finds a file with matching title" do
+        expect(file_list.files).to respond_to(:find)
+        file_names = file_list.files.map{ |f| f.name }
+        expect(file_names).to include(service.spreadsheet_title)
+      end
+    end
 
     describe "#file_list" do
       let(:request_options) { {q: "mimeType='application/vnd.google-apps.spreadsheet'", order_by: "createdTime desc", page_size: 25} }
